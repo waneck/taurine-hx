ad22c87ee478abd40e70ebe801c6a3979b05fa1e
Cauê Waneck <waneck@gmail.com>
2014-05-05 14:12:58 -0300

    merge

--- test/taurine/tests/Test.hx
+++ test/taurine/tests/Test.hx
@@ -17,7 +17,6 @@ class Test
 	{
 		var runner = new Runner();
 
-<<<<<<< HEAD
 		runner.addCase(new taurine.tests.ds.LstTests());
 // 		runner.addCase(new PathTests());
 // 		runner.addCase(new GlobTests());
@@ -31,30 +30,6 @@ class Test
 // 		runner.addCase(new taurine.tests.math.MatrixTests());
 // 		runner.addCase(new taurine.tests.math.QuatTests());
 		runner.addCase(new taurine.tests.react.ReactTests());
-=======
-		var p = Path.dot / "a" / "b" / "" / "c";
-		trace(p);
-		trace(p + "b");
-		trace(p ^ "b");
-		trace(p.normalize());
-		trace(p.dirname());
-		trace(p.dirname().dirname());
-		trace(p.dirname().dirname().dirname());
-		trace(p.dirname().dirname().dirname().dirname());
-		trace(p.dirname().dirname().dirname().dirname().dirname());
->>>>>>> path general api - starting documentation
-
-// 		runner.addCase(new PathTests());
-// 		runner.addCase(new GlobTests());
-// 		runner.addCase(new UriTests());
-// 		runner.addCase(new taurine.tests.UInt8Tests());
-// 		runner.addCase(new taurine.tests.mem.RawMemTests());
-// #if js
-// 		runner.addCase(new taurine.tests.mem.RawMemTests.RawMemTestsBackwards());
-// 		runner.addCase(new taurine.tests.mem.RawMemTests.RawMemTestsArray());
-// #end
-// 		runner.addCase(new taurine.tests.math.MatrixTests());
-// 		runner.addCase(new taurine.tests.math.QuatTests());
 //
 // 		// var report = Report.create(runner);
 // 		var report = new utest.ui.text.PrintReport(runner);
